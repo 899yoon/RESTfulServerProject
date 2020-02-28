40431e833330935ee772b1affb9cbe33161c0258
MOONSEONGJE <61142763+MOONSEONGJE@users.noreply.github.com>
2020-02-28 12:41:48 +0900

    input page

--- src/main/java/com/sdinfo/smarthome/rest/refrigerator/controller/InputContorller.java
+++
@@ -1,5 +0,0 @@
-package com.sdinfo.smarthome.rest.refrigerator.controller;
-
-public class InputContorller {
-
-}
--- src/main/java/com/sdinfo/smarthome/rest/refrigerator/mapper/AircleanerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sdinfo.smarthome.rest.refrigerator.mapper;
-
-import java.util.List;
-
-import com.sdinfo.smarthome.rest.refrigerator.domain.AircleanerVo;
-
-public interface AircleanerMapper {
-	public List<AircleanerVo> getAllAircleaner() throws Exception; //TBL_AIRCLEANER 조회
-}
